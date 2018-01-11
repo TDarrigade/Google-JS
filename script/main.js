4b672a26a69e6e380ae5730114fd3eafce0fe6fd
Thomas 2 <thomas2@MacBook-Pro-de-Thomas.local>
2018-01-11 19:45:55 +0100

    Page google JSv1

--- script/main.js
+++ script/main.js
@@ -1,22 +1,32 @@
+//fonctions
+//___________________________________
+
 function etape1() { 
     document.getElementById('centre').style.display='block' ;
     document.getElementById('myButton').style.display='none' ;
 }
 
 
+function etape2() {
+let recherche = document.querySelector('input').value;
+recherche = recherche.replace(/\s/g,"+");
+window.location.replace("https://www.google.fr/#q="+recherche);	
+}
+
+setTimeout(function() {
+	alert("Viens voir maman sur www.unzeste.com") ; } , 10000);
+
+
+//evenements
+//___________________________________
+
 myButton.onclick = function(){
     etape1() ;
 }
 
+lancerRecherche.onclick = function(){
+	etape2();
 
-let recherche = document.getElementById('bouton')
-
-
-recherche.onclick = function(){
-let  monBouton = recherche.getattribute('value');
-//if (monBouton === "test") {recherche 
-
-//}
-console.log(monBouton)
 }
+
 
